ed6ad27b55d67a88e32ceece59ffdec4dbeabdce
Mary Farraj <125921886+MaryGFarraj@users.noreply.github.com>
2024-02-09 14:41:26 -0500

    Delete javas.final.paletteproject1.js

--- javas.final.paletteproject1.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-    // Your JavaScript code goes here
-});
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Your existing code for the fade-in effect
-});
