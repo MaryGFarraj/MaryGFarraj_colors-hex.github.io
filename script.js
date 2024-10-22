eabd0e5c0c44e846283d9e8ba530295314a02857
Mary Farraj <125921886+MaryGFarraj@users.noreply.github.com>
2024-10-22 12:44:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
-    // Your JavaScript code goes here
+    
 });
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Your existing code for the fade-in effect
+    
 });
